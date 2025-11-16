2d43cf1281c976e70e3d75c2bdaf65adf130f3b8
Johnwincherbachgamingsupheroiamthehulk <matthewliualt@gmail.com>
2025-11-16 16:41:06 +0000

    asdasdasdas

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const clicksound = new Audio("sounds/mixkit-modern-click-box-check-1120.wav");
+const zonesound = new Audio("sounds/soft-kalimba-one-note_C.wav");
+
+// create matter engine, render, world, bodies for physics engine
 const Engine = Matter.Engine,
       Render = Matter.Render,
       World = Matter.World,
@@ -11,32 +15,105 @@ const render = Render.create({
     engine: engine,
     options: {
         width: 1000,
-        height: 1200,
+        height: 1100,
         wireframes: false,
-        background: "black"
+        background: "#112233ff"
     }
 });
 
-engine.world.gravity.y = 0.3
 
-const numRows = 12;   // total number of rows
-const xStep = 80;     // horizontal spacing between pegs
-const yStep = 70;     // vertical spacing between rows
+let playerMoney = 1; // set player money 
 
-for (let row = 0; row < numRows; row++) {
-    const numPegsInRow = row + 1; // row 0 has 1 peg, row 1 has 2, etc.
+
+
+const boardWidth = 1000;
+const boardHeight = 1200;
+
+// set up multipliers for payouts :money:
+const multipliers = [0.5, 0.8, 1, 1.2, 1.5, 2, 5, 10, 5, 2, 1.5, 1.2, 1, 0.8, 0.5];
+const numofmultipliers = multipliers.length;
+const multiplierwidth = boardWidth / numofmultipliers;
+const multiplierheight = 60;
+
+// creating zones 
+for (let index = 0; index < numofmultipliers; index++) {
+    const x = multiplierwidth / 2 + multiplierwidth * index;
+    const y = boardHeight - multiplierheight * 3;
+
+    const zone = Bodies.rectangle(
+        x,
+        y,
+        multiplierwidth,
+        multiplierheight,
+        {
+            isStatic: true,
+            label: "zone",
+            multiplier: multipliers[index],
+            render: { visible : false},
+            multiplierIndex: index
+        }
+    );
+
+    World.add(world, zone);
+
+    // adding text to zones to display their respective text 
+
+    const label = document.createElement("div");
+    label.innerText = `${multipliers[index]}x`;
+    label.dataset.index = index;
+    label.classList.add("bounceanim");
+    label.style.fontFamily = "Brush Script MT, cursive";
+    label.style.position = "absolute";
+    label.style.left = `${x - multiplierwidth / 2}px`;
+    label.style.top = `${y - multiplierheight / 2}px`;
+    label.style.width = "58px";
+    label.style.height = `${multiplierheight}px`;
+    label.style.color = "black";
+    label.style.display = "flex";
+    label.style.alignItems = "center";
+    label.style.justifyContent = "center";
+    label.style.fontWeight = "bold";
+    label.style.backgroundColor = "red";
+    label.style.borderRadius = "10px";
+    label.style.border = "2px solid black"; 
+    document.getElementById("plinko-area").appendChild(label);
+
+
+    /* coloring depending on multiplier */
+    if (multipliers[index] == 10) {
+        label.style.backgroundColor = "#ffef5eff";
+    } else if (multipliers[index] == 5) {
+        label.style.backgroundColor = "#ffc830ff";
+    } else if (multipliers[index] == 2) {
+        label.style.backgroundColor = "#ffa724ff";
+    } else if (multipliers[index] == 1.5) {
+        label.style.backgroundColor = "#fc8414ff";
+    } else if (multipliers[index] == 1.2) {
+        label.style.backgroundColor = "#ff5e00ff";
+    } else if (multipliers[index] == 1) {
+        label.style.backgroundColor = "#ff3c00ff";
+    }
+}
+
+engine.world.gravity.y = 0.2; // higher = stronger, lower = very spacey and cool
+
+const numRows = 16; 
+const xStep = 55;  
+const yStep = 50;    
+
+for (let row = 1; row < numRows; row++) {
+    const numPegsInRow = row + 2;
     const totalRowWidth = (numPegsInRow - 1) * xStep;
-    const offsetX = (1000 - totalRowWidth) / 2; // center the row
+    const offsetX = (1000 - totalRowWidth) / 2; 
 
     for (let col = 0; col < numPegsInRow; col++) {
         let x = offsetX + col * xStep;
 
-        // optional: stagger every other row slightly
-        if (row % 2 === 1) x += xStep * 0.01;
+
 
         const y = 150 + row * yStep;
 
-        const peg = Bodies.circle(x, y, 7, {
+        const peg = Bodies.circle(x, y, 6, {
             isStatic: true,
             render: { fillStyle: "white" }
         });
@@ -44,25 +121,127 @@ for (let row = 0; row < numRows; row++) {
     }
 }
 
-// reference your board width
-const boardWidth = 1000;
-const boardHeight = 1200;
 
-const minX = 450
-const maxX = 550
-// "Drop Ball" button
+// range of where the balls drop on x axis
+const minX = 400
+const maxX = 600
+
+
 const dropBallBtn = document.getElementById("dropballbtn");
 
+
+// drop the ball & creating the ball
+
 dropBallBtn.addEventListener("click", () => {
+    clicksound.currentTime = 0;
+    clicksound.play();
+
+    // uhh checking stuff so it dont break
+    let betAmount = parseFloat(document.getElementById("moneyinpt").value);
+    if (isNaN(betAmount) || betAmount <= 0) {
+        alert("enter a VALID bet amount :$ !!!")
+        return;
+    }
+
+    if (betAmount > playerMoney) {
+        alert("Not enough money :( !!");
+        return;
+    }
+
+
+    playerMoney -= betAmount;
+    document.getElementById("money").innerText = `$${playerMoney.toFixed(2)}`;
     let randomX = minX + Math.random() * (maxX - minX)
-    const ball = Bodies.circle(randomX, 50, 15, {
-        restitution: 0.8,             // bounciness
-        render: { fillStyle: "green" } // color
+    const ball = Bodies.circle(randomX, 140, 12, {
+        friction: 0,
+        frictionAir: 0.01,
+        restitution: 1,             // bounciness
+        density: 0.005,
+        render: { fillStyle: "red" }, 
+        label: "ball",
+        bet: betAmount
     });
 
     World.add(world, ball);
 });
 
 
+Matter.Events.on(engine, 'collisionStart', function(event) {
+    event.pairs.forEach(pair => {
+        const { bodyA, bodyB } = pair;
+
+        let ball = bodyA.label === "ball" ? bodyA : bodyB.label === "ball" ? bodyB : null;
+        let zone = bodyA.label === "zone" ? bodyA : bodyB.label === "zone" ? bodyB : null;
+
+        if (ball && zone) {
+
+            // very LONG line of playing notes depending on what multiplier you hit
+
+            if (zone.multiplier == 10) {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_F_minor__bpm_104 (1).mp3");
+                sound.play();
+            } else if (zone.multiplier == 5) {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_E♭_minor__bpm_104 (1).mp3");
+                sound.play();
+            } else if (zone.multiplier == 2) {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_D_minor__bpm_104.mp3");
+                sound.play();
+            } else if (zone.multiplier == 1.5) {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_C_minor__bpm_104.mp3");
+                sound.play();
+            } else if (zone.multiplier == 1.2) {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_B_minor__bpm_104.mp3");
+                sound.play();
+            } else if (zone.multiplier == 1) {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_B♭_minor__bpm_104.mp3");
+                sound.play();
+            } else {
+                const sound = new Audio("sounds/soft-kalimba-one-note_C_G_minor__bpm_104.mp3");
+                sound.play();
+            }
+
+            let profitorlose = ball.bet * zone.multiplier;
+            playerMoney += profitorlose;
+
+
+            document.getElementById("money").innerText = `$${playerMoney.toFixed(2)}`;
+
+            World.remove(world, ball);
+            
+
+
+            const label = document.querySelector(`#plinko-area div[data-index='${zone.multiplierIndex}']`);
+            if (label) {
+                label.style.transform = "translateY(10px)";
+
+                setTimeout(() => {
+                    label.style.transform = "translateY(0)";
+                }, 200);
+            }
+        }
+    })
+})
+
+
+// run the engine so it displays the game
 Engine.run(engine);
 Render.run(render);
+
+
+
+
+
+
+/* BORDERS TO PREVENT BALLS FROM ESCAPING!!!
+*/
+const borderleft = Bodies.rectangle(10 / 2, 1200 / 2,  10, 1200, {
+    isStatic:  true,
+    render: {visible : false}
+});
+World.add(world, borderleft);
+
+const borderright = Bodies.rectangle(1000 - 10/2, 1200 / 2,  10, 1200, {
+    isStatic:  true,
+    render: {visible : false}
+});
+World.add(world, borderright);
